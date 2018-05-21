9d67724800b139dfeaea082879ff37944f25ec78
Mike Garuccio <mgaruccio@gmail.com>
2018-05-21 19:35:35 -0400

    expanded code to be more clear

--- python-pangram/pangram.py
+++ python-pangram/pangram.py
@@ -1,6 +1,14 @@
-# So this version is more of a joke than anything, wanted to see if I could do it in one line
-# It works all right but honestly it's probably not the easiest thing to explain compared with using an array
+import string
 
 
 def is_pangram(sentence):
-    return len([x for x in set(sentence.lower()) if x.isalpha()]) == 26
+    # Convert all letters in the string to lowercase
+    lowerCaseSentence = sentence.lower()
+    # Use the all-lowercase string as the input for creating a new set of unique characters
+    uniqueLetters = set(lowerCaseSentence)
+    # Filter the set to include only letters, removing all numbers and symbols
+    alphaOnly = [x for x in uniqueLetters if x.isalpha()]
+    # determine the length of the resulting set
+    arrayLength = len(alphaOnly)
+    # return the result of a comparison between the length of the array and number of letters in the alphabet
+    return arrayLength == len(string.ascii_lowercase)
